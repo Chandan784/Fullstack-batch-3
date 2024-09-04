237df175caf3879a3bd5e1e3ff54a42feeeca44a
chandan <chandansamantaray784@gmail.com>
2024-09-04 23:04:58 +0530

    js datatype and variable

---
+++ class20_js_introduction/index.js
@@ -0,0 +1,43 @@
+console.log("chandan samantaray");
+
+//synatx
+// data type and variable
+// operators  - arithmatic, logical, conditional , assignment, bitwise, ternary
+//conditional statement - if and else , else if lader, swicth case
+
+//loop - for , do while, while, for of , for in, for each
+
+//function -  defination , return type, parameter , argument  , arrow function , function expression , anonymous function,
+
+//array - push, pop, shift,unshift, map, filter , reduce , slice , spice, fill, find, some
+
+//string
+
+//object -- key, value, computed properties,
+
+// array and object destructuring
+//dom -
+
+//advance jabascripit
+
+//sycn and async
+
+// api calling
+
+// promise
+// clouser
+// hoisting
+
+//call backs
+//async await
+
+//object oriented programming
+//class, object, inheritanc, polymorphism, encapuslation, abstraction, interface, constructor
+//prototype
+
+//DSA
+
+//array and string, arraylist and linkedlist,
+//linear search and binary search
+//sort-
+// time and space complecity
---
+++ class21_js_datatype_variable/index.js
@@ -0,0 +1,88 @@
+console.log("applute");
+
+//data type and variable
+
+// programming code ---  complie --- exe 0,1
+
+// mumber
+// String
+// boolean
+// array
+// object
+// null
+// NaN
+// undefined
+//symbol
+
+// let x = null;
+// console.log(x);
+
+//memoey unit - bit - 0,1
+
+//  1gb = 1024mb
+//  1mb = 1024kb
+//  1kb = 1024bytes
+//  1 byte = 8bit
+// 1 bit = 0/1
+
+//  ram -- 1gb ram --
+
+// char =
+
+// interger
+// string
+
+// // let x = "c"
+// let name = "chandan samantaray"
+
+// let x = 2;
+
+// let , var , const
+
+//camel case
+
+// let myName = "chandan"
+
+// //pascal case
+
+// let MyName = "chandan"
+
+// let x = 2;
+// var y = 10;
+// const z = 11;
+
+// z = 13;
+
+// var num = 2;
+// console.log(num);
+
+// {
+//   console.log(num);
+//   var num3 = 111;
+//   let num2 = 20;
+//   console.log(num2);
+// }
+
+// console.log(num3);
+
+// function findSqr() {
+//   var x = 2;
+// }
+
+// console.log(x);
+
+//
+
+let x = 2;
+
+let z;
+
+z = 20;
+
+let z = 20
+
+let arr = [2,3,4,5,]
+
+stack -- arr
+
+heap -- [2,3,4,5,]
