0e8148343c3b4042cfeeb8cde5254862325d5ab3
chandan <chandansamantaray784@gmail.com>
2024-09-10 23:15:38 +0530

    js loop

---
+++ class24_js_looping/index.js
@@ -0,0 +1,66 @@
+// console.log("hii");
+
+// let num = 2;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(2 * i);
+// }
+
+// we have to find all the even number betn 1 to 100
+// let count = 0;
+// for (let i = 1; i < 100; i++) {
+//   if (i % 2 != 0) {
+//     count = count + 1;
+//   }
+// }
+
+// console.log(count);
+
+// prime number
+
+// 10
+
+// 1*10 = 10
+// 2*5 = 10
+
+// 1, 2, 5, 10
+
+// 3
+
+// 1* 3 = 3
+
+// let num = 14;
+// 1,2,3,4,5,6,7,8,9,10,11,12,13,14
+// 1,2,7,14
+// 1,2,3,4,5,6,7,8,9,10
+
+// let count = 0;
+// for (let i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     count = count + 1;
+//   }
+// }
+// console.log(count);
+
+// if (count <= 2) {
+//   console.log("this number is prime number");
+// } else {
+//   console.log("this number is not prime number");
+// }
+
+// let num = 3;
+
+// let i = 1;
+
+// while (i < 11) {
+//   console.log(num * i);
+//   i++;
+// }
+// let num = 4;
+// let i = 1;
+// do {
+//   console.log("hii");
+
+//   console.log(num * i);
+//   i++;
+// } while (i < 11);
