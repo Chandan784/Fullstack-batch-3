97a3a6e048a0f88c5159a41c816e94b1ec8fd11b
chandan <chandansamantaray784@gmail.com>
2024-10-01 22:11:07 +0530

    array all methods

---
+++ class35_array_methods/index.js
@@ -0,0 +1,50 @@
+//find method
+
+// let num = [1, 2, 3, 4, 5];
+
+// let ans = num.find((e) => {
+//   return e > 2;
+// });
+
+// console.log(ans);
+
+// let studentNames = ["chandan", "tapan", "dilal"];
+
+// let data = studentNames.find((e) => {
+//   return e.includes("a");
+// });
+// console.log(data);
+
+//every method
+// let numbers = [2, 4, 5, 566, 7];
+
+// let myData = numbers.every((e) => {
+//   return e > 10;
+// });
+
+// console.log(myData);
+
+//some method
+// let arr = [2, 4, 5, 6, 7, 8];
+
+// let myAns = arr.some((e) => {
+//   return e > 5;
+// });
+
+// console.log(myAns);
+
+//fill method
+
+// let arr = [2, 3, 4, 5, 6];
+
+// arr.fill(10);
+// console.log(arr);
+
+// let data = ["c", "a", "t"];
+
+// let value = data.join("");
+
+// console.log(value.length);
+
+let username = "12345";
+console.log(username.split("3"));
