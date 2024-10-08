6879b6cc9998ed0dd114b9034a49d584301b495e
chandan <chandansamantaray784@gmail.com>
2024-10-08 22:27:46 +0530

    internal working of js

---
+++ class40_internal_working/index.js
@@ -0,0 +1,55 @@
+// console.log("hii");
+// console.log("hlw");
+
+// lep;
+
+// let x = 2;
+// console.log(x);
+
+// let x = 3;
+
+// console.log(this);
+
+// console.log(user);
+
+// let user = "chandan";
+
+// console.log(user);
+
+// console.log(demo);
+
+// function demo() {
+//   console.log("demo");
+// }
+
+// this,var,let,const,function
+
+// console.log(y);
+// console.log(x);
+
+// let x = 2;
+// var y = 4;
+
+// console.log(fun1);
+
+// function fun1() {
+//   console.log("i am fun1");
+// }
+
+// console.log(ans,"first");
+
+// console.log(ans);
+
+// function fun1(a, b) {
+//   console.log(arguments);
+
+//   let c = a + b;
+
+//   return c;
+// }
+
+// var ans = fun1(2, 3, 5);
+
+// console.log(ans);
+
+// console.log(this);
